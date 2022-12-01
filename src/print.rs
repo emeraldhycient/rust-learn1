3a720c4bc1fde6359e59760793b407a3065ff4d9
emeraldhycient <igwezehycient86@gmail.com>
2022-12-01 21:18:05 +0100

    first commit

---
+++ src/print.rs
@@ -0,0 +1,15 @@
+pub fn run() {
+    println!("hello motherfucker");
+    // formatting
+    println!("something : {}", 234);
+    // passing multiple values
+    println!("value: {} and we have value {}", "1", 2);
+
+    //positional argumengts(pass the index of the values)
+    println!(
+        " {0} is from {1} and  {0} loves to {2}",
+        "hycient", "earth", "code"
+    );
+
+    // named arguments
+}
